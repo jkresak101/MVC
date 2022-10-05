8451e9e12653043930578121f494f82868964fda
jkresak101 <jkresak101@gmail.com>
2022-10-05 11:14:37 -0700

    update

--- HelloMVC/HelloMVC.Tests/HomeControllerTests.cs
+++ HelloMVC/HelloMVC.Tests/HomeControllerTests.cs
@@ -1,11 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Web.Mvc;
+using ViewResult = Microsoft.AspNetCore.Mvc.ViewResult;
+
 namespace HelloMVC.Tests
 {
-    public class UnitTest1
+
+    public class HomeControllerTests
     {
         [Fact]
-        public void Test1()
+        public void Index_Returns_ViewResult()
         {
+            var controller = new HomeController();
 
+            var result = controller.Index();
+
+            Assert.IsType<ViewResult>(result);
         }
     }
 }
---
+++ HelloMVC/HelloMVC/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            return null;
+        }
+    }
+}
